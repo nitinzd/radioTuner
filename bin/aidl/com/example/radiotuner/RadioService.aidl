15ee6ec4d395739d186b1eab0b4cb373a58c7dc3
nitinzd <Nitin@Nitin-PC.(none)>
2017-06-11 00:39:08 +0530

    Added Services

--- bin/aidl/com/example/radiotuner/RadioService.aidl
+++ bin/aidl/com/example/radiotuner/RadioService.aidl
@@ -1,6 +1,12 @@
 package com.example.radiotuner;
 
-interface RadioService {
+interface RadioService {	
+// Will check whether service is available or not
+// Return true if service available. Else false
+boolean isServiceAvailable(String sName);
 
-
+//Will connect to specified API
+//It could be AM o		r FM
+//Return response according to service
+String linkAPI(String sApiName);
 }
--- gen/com/example/radiotuner/RadioService.java
+++ gen/com/example/radiotuner/RadioService.java
@@ -42,6 +42,26 @@ case INTERFACE_TRANSACTION:
 reply.writeString(DESCRIPTOR);
 return true;
 }
+case TRANSACTION_isServiceAvailable:
+{
+data.enforceInterface(DESCRIPTOR);
+java.lang.String _arg0;
+_arg0 = data.readString();
+boolean _result = this.isServiceAvailable(_arg0);
+reply.writeNoException();
+reply.writeInt(((_result)?(1):(0)));
+return true;
+}
+case TRANSACTION_linkAPI:
+{
+data.enforceInterface(DESCRIPTOR);
+java.lang.String _arg0;
+_arg0 = data.readString();
+java.lang.String _result = this.linkAPI(_arg0);
+reply.writeNoException();
+reply.writeString(_result);
+return true;
+}
 }
 return super.onTransact(code, data, reply, flags);
 }
@@ -60,6 +80,60 @@ public java.lang.String getInterfaceDescriptor()
 {
 return DESCRIPTOR;
 }
+// Will check whether service is available or not
+// Return true if service available. Else false
+
+@Override public boolean isServiceAvailable(java.lang.String sName) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+boolean _result;
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeString(sName);
+mRemote.transact(Stub.TRANSACTION_isServiceAvailable, _data, _reply, 0);
+_reply.readException();
+_result = (0!=_reply.readInt());
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+return _result;
+}
+//Will connect to specified API
+//It could be AM o		r FM
+//Return response according to service
+
+@Override public java.lang.String linkAPI(java.lang.String sApiName) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+java.lang.String _result;
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeString(sApiName);
+mRemote.transact(Stub.TRANSACTION_linkAPI, _data, _reply, 0);
+_reply.readException();
+_result = _reply.readString();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+return _result;
 }
 }
+static final int TRANSACTION_isServiceAvailable = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+static final int TRANSACTION_linkAPI = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
+}
+// Will check whether service is available or not
+// Return true if service available. Else false
+
+public boolean isServiceAvailable(java.lang.String sName) throws android.os.RemoteException;
+//Will connect to specified API
+//It could be AM o		r FM
+//Return response according to service
+
+public java.lang.String linkAPI(java.lang.String sApiName) throws android.os.RemoteException;
 }
